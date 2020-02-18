226bdcb8dd860a4951f5299c960d0648d66c0ca9
Richard Kwon <rkminjae@gmail.com>
2020-02-18 17:09:41 +1300

    Add semicolons to statements

--- game.js
+++ game.js
@@ -4,23 +4,23 @@
 document.addEventListener('DOMContentLoaded', start)
 
 function start () {
-  bindEventListeners(document.getElementsByClassName('board')[0].children)
+  bindEventListeners(document.getElementsByClassName('board')[0].children);
 }
 
 function bindEventListeners (dots) {
   for (var i = 0; i < dots.length; i++) {
     // BIND YOUR EVENT LISTENERS HERE
     // The first one is provided for you
-    dots[i].addEventListener('contextmenu', makeGreen)
+    dots[i].addEventListener('contextmenu', makeGreen);
     dots[i].addEventListener("click", makeBlue);
     dots[i].addEventListener("dblclick", hide);
   }
 }
 
 function makeGreen (evt) {
-  evt.preventDefault()
-  evt.target.classList.toggle('green')
-  updateCounts()
+  evt.preventDefault();
+  evt.target.classList.toggle('green');
+  updateCounts();
 }
 
 // CREATE FUNCTION makeBlue HERE
@@ -61,11 +61,11 @@ function updateCounts () {
   }
 
   // Once you've done the counting, this function will update the display
-  displayTotals(totals)
+  displayTotals(totals);
 }
 
 function displayTotals (totals) {
   for (var key in totals) {
-    document.getElementById(key + '-total').innerHTML = totals[key]
+    document.getElementById(key + '-total').innerHTML = totals[key];
   }
 }
